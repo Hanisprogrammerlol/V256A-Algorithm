43493e15ed1b683330e874592796fbb2e5aa8aee
Hanisprogrammerlol <78644866+Hanisprogrammerlol@users.noreply.github.com>
2022-08-27 15:48:24 +0700

    Delete V256A directory

--- V256A/core.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#ifndef coredothpp
-#define coredothpp
-#include "core.cpp"
-#endif // !core.hpp
-
-static void V256A_GenerateHash(const char* msg, uint16_t rotation, uint16_t xor_rotator, uint16_t hash_obsfuscation);
