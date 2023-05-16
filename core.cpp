6024648ee8014d07f233fb641de0b12c0bfb427f
Hanisprogrammerlol <78644866+Hanisprogrammerlol@users.noreply.github.com>
2023-05-16 20:36:10 +0700

    Revision 1.4.5.bt1

--- core.cpp
+++ core.cpp
@@ -30,6 +30,7 @@
 * thank you for sha256 and argon2 creator
 * 
 * known issues:
+* -incompatible for 32bit platforms
 * 
 * fixed issues:
 * -performance is better
@@ -37,26 +38,37 @@
 * -calcchunk performance is better
 * -fixed output will return blank string
 * -(solved) because this core used sprintf the variable sometimes will cause buffer overflow (i didnt use sprintf_s because of some issues)
-* -changed malloc into calloc because of undefined behaviour of hashing
+* -changed malloc into calloc because of undefined behaviour when hashing
 * 
 * current version:
-* 1.4.4 Revision
+* 1.4.5.bt1 Revision
 * 
 * warning!
 * V256A has been tested on msvc2019 and g++, but not tested on clang and other compiler
-* minimum ram size is 4kb
+* minimum ram size is 2048kb/2mb, recommended 8mb/32mb
+* supported for x86 and x64 platforms, armv7 support coming soon!
 * 
 */
-
-#include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include "core.hpp"
 
 //disable C4996 error for msvc compilers
 #ifdef _MSVC_LANG
 #pragma warning(disable: 4996)
 #endif
+
+#pragma optimize("O3, inline", on)
+#ifdef _M_IX86
+#define V256A_OPT __cdecl
+#elif _M_X64
+#define V256A_OPT __stdcall
+#elif _M_ARM
+//Armv7 support comming soon
+#endif
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <immintrin.h>
+#include "core.hpp"
 
 //V256A Struct Initialization
 V256A_Constructor V256A_Cons;
@@ -80,13 +92,13 @@ void V256A_Init(void) {
 //Chunk Calculator for V256A must call V256A_Init first before calling function
 void V256A_CalcChunks(uint8_t asciicode) {
     //before calcchunks, call V256A_Init(); first to initialize default data
-    const uint64_t ch[10]{
+    register constexpr uint64_t ch[10]{
         0xfd2c64f92d42cfab, 0x25581188432fcc78, 0xf0a78705d3d0dcb1, 0xbe2339b2d97f789f,
         0xe65043773f9c3a9e, 0x4ec4de1fee1403f8, 0xc428ee3cbc51eba8, 0xfddaa39125bff3eb,
         0x2fe2133270548bf8, 0x47ad172ee39dc3aa
 
     };
-    uint32_t *temp1 = (uint32_t*)calloc(10, sizeof(uint32_t));
+    register uint64_t *temp1 = (uint64_t*)calloc(10, sizeof(uint64_t));
     for (uint32_t i = 0; i < 10; ++i) {
         temp1[i] += ch[i];
         temp1[i] += temp1[i] * 4;
@@ -100,7 +112,7 @@ void V256A_CalcChunks(uint8_t asciicode) {
 };
 
 void V256A_Digest(void) {
-    const V256A_Uint16_t v[64]{
+    register constexpr V256A_Uint16_t v[64]{
         0xc593, 0x29ae, 0x19fc, 0x70b0, 0x2dba,
         0x9a32, 0x290d, 0x8ec7, 0xa9f8, 0xb021,
         0x359a, 0xa00e, 0x7825, 0xcdb3, 0x52c4,
@@ -120,17 +132,39 @@ void V256A_Digest(void) {
     }
 }
 
+//additional salting system for V256A
+void V256A_Mixins(char* hash, char* output, uint8_t mixin_mapping[6]) {
+    const uint8_t mixin_table[24] = {34, 38, 36, 43, 47, 63,
+                               34, 38, 36, 43, 47, 63, 
+                               34, 38, 36, 43, 47, 63, 
+                               34, 38, 36, 43, 47, 63};
+    register char* temp = (char*)calloc(2048, sizeof(char));
+    register char temp2[2048] = { 0x0 };
+    for (int i = 0; i < 24; ++i) {
+        V256A_Cons.curr_mixins[i] = hash[i];
+    }
+    for (int i = 0; i < 6; ++i) {
+        V256A_Cons.curr_mixins[mixin_mapping[i]] = mixin_table[mixin_mapping[i]];
+    }
+    for (uint16_t i = 0; i < 24; ++i) {
+        sprintf(temp, "%I64x", V256A_Cons.curr_mixins[i]);
+        strcat(temp2, &temp[i]);
+    }
+    free(temp);
+    memcpy(output, temp2, 24);
+};
+
 //Generate V256A Hash But Call V256A_Init() and V256A_CalcChunks() first
-void V256A_GenerateHash(const char *msg, uint16_t rotation, uint16_t xor_rotator, uint16_t hash_obsfuscation) {
-    V256A_Text *tempmsg = (V256A_Text*)calloc(16, sizeof(V256A_Text));
-    V256A_Uint64_t datstrhash = 0;
+void V256A_OPT V256A_GenerateHash(const char *msg, uint16_t rotation, uint16_t xor_rotator, uint16_t hash_obsfuscation) {
+    V256A_Text *tempmsg = (V256A_Text*)calloc(24, sizeof(V256A_Text));
+    register V256A_Uint64_t datstrhash = 0;
     V256A_Uint64_t mlen = 0;
     //checking for parameters
     if (hash_obsfuscation < 128) {
         return throw "Hash Obsfuscation Must Be Up To 128bit";
     }
     if (hash_obsfuscation > 512) {
-        return throw "Hash Obsfuscation Must Be Smaller Than 1024bit";
+        return throw "Hash Obsfuscation Must Be Smaller Than 512bit";
     }
     if (rotation < 1) {
         return throw "Rotation Must Be Up To 1";
@@ -159,7 +193,7 @@ void V256A_GenerateHash(const char *msg, uint16_t rotation, uint16_t xor_rotator
     //overwrite data
     memcpy(tempmsg, msg, sizeof(tempmsg));
     //because of some changes V256A will have 16 different hex for better performance
-    for (uint32_t i = 0; i < 16; i++) {
+    for (uint16_t i = 0; i < 24; i++) {
         datstrhash = tempmsg[i] >> i * rotation;
         datstrhash = datstrhash << 2 >> 4 << 1 >> rotation + hash_obsfuscation;
         datstrhash = datstrhash ^ xor_rotator + 1;
@@ -177,19 +211,63 @@ void V256A_GenerateHash(const char *msg, uint16_t rotation, uint16_t xor_rotator
         V256A_Cons.curr_hash[i] += datstrhash / 6;
     }
     free(tempmsg);
+    if (strlen(msg) > 24) { V256A_Cons.mixin_table[0] = strlen(msg) - strlen(msg) + 6; }
+    V256A_Cons.mixin_table[1] = int(V256A_Cons.curr_hash[0]); V256A_Cons.mixin_table[2] = int(V256A_Cons.curr_hash[5]) + 2;
+    V256A_Cons.mixin_table[3] = int(&msg[0]); V256A_Cons.mixin_table[4] = int(&msg[strlen(msg) - 1]); V256A_Cons.mixin_table[5] = xor_rotator/4;
     return;
 };
 
-//last process to call before V256A_Sweep() and after V256A_GenerateHash()
-void V256A_ProcessHash(char* output) {
-    char *temp = (char*)calloc(16, sizeof(char));
-    char temp2[512] = "";
-    for (uint16_t i = 0; i < 16; ++i) {
-        sprintf(temp, "%llx", V256A_Cons.curr_hash[i]);
-        strcat(temp2, temp);
+//reworked V256A prototype for AVX platforms
+void V256A_OPT V256A_GenerateHash_AVX(const char* msg, uint16_t rotation, uint16_t xor_rotator, uint16_t hash_obsfuscation) {
+    V256A_Text* tempmsg = (V256A_Text*)calloc(24, sizeof(V256A_Text));
+    //V256A_Uint64_t datstrhash = 0;
+    register __m128i datstrhash = _mm_set1_epi64x(0);
+    __m128i mlen = _mm_set1_epi64x(0);
+    for (uint16_t i = 0; i < 24; i++) {
+        datstrhash.m128i_u64[0] = tempmsg[i] >> i * rotation;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] << 2 >> 4 << 1 >> rotation + hash_obsfuscation;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] ^ xor_rotator + 1;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] >> mlen.m128i_u64[0] << 4 >> 2 ^ xor_rotator;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] ^ 64;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] << 2;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] | 2;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] >> 4;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] ^ 4 ^ 3 ^ 2 >> 1 & 4 ^ 5 | 3 << 3 >> 4 << 6 ^ 2 >> 1;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] + V256A_Cons.rfactor[i] >> 2;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] / V256A_Cons.digest_size[i];
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] + V256A_Cons.chunks[i] / 2;
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] ^ V256A_Cons.digest_size[i + xor_rotator];
+        datstrhash.m128i_u64[0] = datstrhash.m128i_u64[0] * strlen(msg);
+        V256A_Cons.curr_hash[i] += datstrhash.m128i_u64[0] / 6;
     }
-    free(temp);
-    memcpy(output, temp2, strlen(temp2) + 1);
+    free(tempmsg);
+    if (strlen(msg) > 24) { V256A_Cons.mixin_table[0] = strlen(msg) - strlen(msg) + 6; }
+    V256A_Cons.mixin_table[1] = int(V256A_Cons.curr_hash[0]); V256A_Cons.mixin_table[2] = int(V256A_Cons.curr_hash[5]) + 2;
+    V256A_Cons.mixin_table[3] = int(&msg[0]); V256A_Cons.mixin_table[4] = int(&msg[strlen(msg) - 1]); V256A_Cons.mixin_table[5] = xor_rotator / 4;
+    return;
+}
+
+//last process to call before V256A_Sweep() and after V256A_GenerateHash()
+//x86 use __cdecl and x64 use __stdcall
+void V256A_OPT V256A_ProcessHash(char* output) {
+    register char* temp = (char*)calloc(24, 24); //every memory count holds 24 byte of hex
+    register char* temp2 = (char*)calloc(24, 24);
+    register constexpr uint64_t rnf[24] = {
+        0xb5f0d8a276e1217e, 0x3b9a344c204773bb, 0x21c90dd3947d20e1, 0xeffd0ff46673395d,
+        0xf659babc19db37e0, 0x8bfe034635a03780, 0x3794e110a37cbce3, 0xf06d93c33c462c1e,
+        0x83848946966d22ee, 0xb10308c3b2f5bc8b, 0xaeea9fc976b4730e, 0x41e8e7ffa44e2d32,
+        0x8fbd0d04fd765535, 0x730f76da6f27b69b, 0x7addd82abb8a328e, 0xda2469f78f9a077a,
+        0xc9172144bbe4be76, 0xc5056fa23d95cc26, 0xc674ab49cd2c5848, 0xd8a71ee2f4873f4b,
+        0x3181afd67b0c3ba7, 0xbda094b8c45aa6e9, 0x6416c8d8583fad4f, 0xb022126829b1b3ea
+    };
+    for (uint16_t i = 0; i < 24; ++i) {
+        sprintf(temp, "%I64x", (uint64_t)(V256A_Cons.curr_hash[i] * V256A_Cons.curr_hash[i] * V256A_Cons.rfactor[i] + V256A_Constants_A + rnf[i]));
+        strcat(&temp2[i], &temp[i]);
+    }
+    //memcpy_s(&output, 48, temp2, 48);
+    strncpy(output, temp2, 48);
+    return;
+    free(temp);free(temp2);
 }
 
 //reset chunks, rfactor, curr_hash, digest_size
@@ -200,6 +278,7 @@ void V256A_Sweep(void) {
     }
     for (uint16_t i = 0; i < 16; ++i) {
         V256A_Cons.curr_hash[i] = 0x0;
+        V256A_Cons.curr_mixins[i] = 0x0;
     }
     for (uint16_t i = 0; i < 64; ++i) {
         V256A_Cons.digest_size[i] = 0x0;
--- core.hpp
+++ core.hpp
@@ -24,6 +24,8 @@
 
 #ifndef CORE_HPP
 #define CORE_HPP
+
+/* These macros only used in Revision 1.4.4 and below
 //V256A_OUTPUTSIZE_S is used on common V256A hashing (hashing with 0-8192 char text length with allocated 256 byte of memory)
 #define V256A_OUTPUTSIZE_S 256
 //V256A_OUTPUTSIZE_B is used on complex V256A hashing (hashing with 8192-16384 char text length with allocated 512 byte of memory)
@@ -32,12 +34,20 @@
 #define V256A_OUTPUTSIZE_H  1024
 //V256A_OUTPUTSIZE_HH is used on very very complex V256A hashing with allocated 2048 byte of memory
 #define V256A_OUTPUTSIZE_HH  2048
+*/
+
+#ifdef _M_IX86
+#define V256A_OUTPUT 48
+#elif _M_X64
+#define V256A_OUTPUT 48
+#endif
+
 #include <iostream>
 
 //V256A Custom Type
 
 //V256A Text Format
-typedef char V256A_Text;
+typedef unsigned char V256A_Text;
 //V256A Uint16_t Format
 typedef unsigned short V256A_Uint16_t;
 //V256A Uint64 Format
@@ -45,6 +55,10 @@ typedef unsigned long long int V256A_Uint64_t;
 
 //Constructor for V256A (rfactor, chunks and prevhash as default is 0x0)
 typedef struct V256A_Constructor {
+	uint8_t mixin_table[6] = {
+		0x0, 0x0, 0x0, 0x0, 0x0,
+		0x0
+	};
 	V256A_Uint64_t rfactor[10] = {
 		0x0, 0x0, 0x0, 0x0, 0x0,
 		0x0, 0x0, 0x0, 0x0, 0x0
@@ -68,24 +82,44 @@ typedef struct V256A_Constructor {
 		0x0, 0x0, 0x0, 0x0, 0x0,
 		0x0, 0x0, 0x0, 0x0
 	};
-	uint64_t curr_hash[16] = {
+	uint64_t curr_mixins[24] = {
 		0x0, 0x0, 0x0, 0x0, 0x0,
 		0x0, 0x0, 0x0, 0x0, 0x0,
 		0x0, 0x0, 0x0, 0x0, 0x0,
-		0x0
+		0x0, 0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x0
+	};
+	uint64_t curr_hash[24] = {
+		0x0, 0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x0
 	};
 };
 
 void V256A_Init(void);
 void V256A_CalcChunks(uint8_t asciicode);
 void V256A_Digest(void);
+void V256A_Mixins(char* hash, char* output, uint8_t mixin_mapping[6]);
 void V256A_GenerateHash(const char* msg, uint16_t rotation, uint16_t xor_rotator, uint16_t hash_obsfuscation);
 void V256A_ProcessHash(char* output);
 void V256A_Sweep(void);
 
+//prototype functions
+void V256A_GenerateHash_AVX(const char* msg, uint16_t rotation, uint16_t xor_rotator, uint16_t hash_obsfuscation);
+
 class V256A {
 	public:
 		static void CreateHash(const char* text, char* output, uint16_t rotation, uint16_t xor_rotator, uint16_t hash_obsfuscation) {
+			V256A_Init();
+			V256A_Digest();
+			V256A_CalcChunks(text[0]);
+			V256A_GenerateHash(text, rotation, xor_rotator, hash_obsfuscation);
+			V256A_ProcessHash(output);
+			V256A_Sweep();
+		}
+		static void __fastcall CreateHash_x86(const char* text, char* output, uint16_t rotation, uint16_t xor_rotator, uint16_t hash_obsfuscation) {
 			V256A_Init();
 			V256A_CalcChunks(text[0]);
 			V256A_Digest();
